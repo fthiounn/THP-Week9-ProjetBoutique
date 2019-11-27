8043a4fb7fa12948bcef6ad1e5f22e6249f98bc6
fthiounn <fthiounn@gmail.com>
2019-11-27 15:09:46 +0100

    fix du bug de fermeture de la modale

--- app/assets/javascripts/modales.js
+++ app/assets/javascripts/modales.js
@@ -11,12 +11,6 @@ $(document).ready(function() {
      e.preventDefault();
      modal.style.display = "block";
   }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target == modal) {
