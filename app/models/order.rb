329cf5c090f0eea1dc8bfebbd7b6a67d2d71f6a9
Maxcouturier <couturier.maxence@gmail.com>
2019-11-27 19:41:29 +0100

    Mailers after_create Order. to be reviewed

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :items, through: :order_items
+  after_create :order_confirmation_email
+
+  def order_confirmation_email
+    UserMailer.order_confirmation_email(self.user, self).now
+  end
+
 end
