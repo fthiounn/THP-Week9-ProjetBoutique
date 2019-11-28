6694ed0dbb921d2eb9e73a4035d843bff3dd226e
Maxcouturier <couturier.maxence@gmail.com>
2019-11-28 12:16:35 +0100

    panier caché if user non conntecté

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,8 @@
 module ApplicationHelper
 	
 	def current_cart_id
-		Cart.where(user_id: current_user.id).id
-	end
-
-	def current_cart_id
     Cart.where(user_id: current_user.id).first.id
-    end
+  end
 
 
 end
