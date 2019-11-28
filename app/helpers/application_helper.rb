d0f9ab629515ef3265bc46fdbfdebfb68ac69d2c
fthiounn <fthiounn@gmail.com>
2019-11-28 15:20:37 +0100

    ajout panier done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-	
-	def current_cart_id
-		Cart.where(user_id: current_user.id).id
-	end
 
-	def current_cart_id
+  def current_cart
+    Cart.where(user_id: current_user.id).first
+  end
+
+  def current_cart_id
     Cart.where(user_id: current_user.id).first.id
-    end
+  end
 
 
 end
