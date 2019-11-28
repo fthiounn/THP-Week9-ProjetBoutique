a80da8b48053768ab0031db278e4f84d4ee315de
fthiounn <fthiounn@gmail.com>
2019-11-28 11:23:31 +0100

    fix duplicate app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,5 @@
 module ApplicationHelper
-	
-	def current_cart_id
-		Cart.where(user_id: current_user.id).id
-	end
-
-	def current_cart_id
+  def current_cart_id
     Cart.where(user_id: current_user.id).first.id
-    end
-
-
+  end
 end
